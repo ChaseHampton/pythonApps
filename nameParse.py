22eac1537381e9e654aae299a2403fa054fae29e
Chase <chase.hampton10@gmail.com>
2019-08-21 20:40:27 -0500

    Moving parse code to function

--- nameParse.py
+++ nameParse.py
@@ -1,31 +1,33 @@
 rawNames = ['John Jacob Jingleheimer Schmidt, Jr.', 'Alan Rickman', 'Bruce Fuckin Willis Jr.', 'Harry Albus Severus James Potter', 'Harry Albus Severus James Potter Jr.', 'Harry Conick, Jr.', 'Adolfo J. Cotilla, Jr., AIA', 'As Many Names As I Want To Have']
 commonSuffixes = ['Jr', 'Jr.', 'jr', 'jr.', 'JR', 'JR.', 'Sr.', 'Sr', 'sr', 'sr.', 'SR', 'SR.']
-
-for name in rawNames:
-    commaParse = name.split(',')
-    try:
-        suffix = commaParse[1]
-    except:
-        suffix = ''
-        pass
-    nameOnly = commaParse[0].split(' ')
-    if len(nameOnly) > 2:
-        if nameOnly[-1] in commonSuffixes:
-            suffix = nameOnly[-1]
-            nameOnly.pop(-1)
-            fName = nameOnly[0]
-            mName = nameOnly[1]
-            lName = ' '.join(nameOnly[2:])
+def nameParse(nameList):
+    for name in nameList:
+        commaParse = name.split(',')
+        try:
+            suffix = commaParse[1]
+        except:
+            suffix = ''
+            pass
+        nameOnly = commaParse[0].split(' ')
+        if len(nameOnly) > 2:
+            if nameOnly[-1] in commonSuffixes:
+                suffix = nameOnly[-1]
+                nameOnly.pop(-1)
+                fName = nameOnly[0]
+                mName = nameOnly[1]
+                lName = ' '.join(nameOnly[2:])
+            else:
+                fName = nameOnly[0]
+                mName = nameOnly[1]
+                lName = ' '.join(nameOnly[2:])
         else:
             fName = nameOnly[0]
-            mName = nameOnly[1]
-            lName = ' '.join(nameOnly[2:])
-    else:
-        fName = nameOnly[0]
-        mName = ''
-        lName = nameOnly[1]
+            mName = ''
+            lName = nameOnly[1]
 
-    print('FirstName: ' + fName)
-    print('MiddleName: ' + mName)
-    print('LastName: ' + lName)
-    print('Suffix: ' + suffix)
+        print('FirstName: ' + fName)
+        print('MiddleName: ' + mName)
+        print('LastName: ' + lName)
+        print('Suffix: ' + suffix)
+
+nameParse(rawNames)
